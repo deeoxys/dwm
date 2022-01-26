c59c62f72a189afab7ee3e7c1728500ae61768cb
r333mo <65601883+r333mo@users.noreply.github.com>
2022-01-26 14:28:06 +0000

    border and new colour

--- config.h
+++ config.h
@@ -2,18 +2,19 @@
 // r333mo config.h  for dwm
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Consolas NF:size=12" };
-static const char dmenufont[]       = "Consolas NF:size=18";
+static const char dmenufont[]       = "Consolas NF:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 // main top bar colour
-static const char col_cyan[]        = "#bd93f9";
+// #bd93f9
+static const char col_cyan[]        = "#f59542";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
