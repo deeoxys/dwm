57788d34e62cc30d523e8b6c64ea2cde2f6e4dd1
r333mo <65601883+r333mo@users.noreply.github.com>
2022-03-03 17:30:20 +0000

    keybinds for zathura and vimb

--- config.h
+++ config.h
@@ -67,11 +67,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *vimb[]  = { "vimb", NULL };
+static const char *zathura[]  = { "zathura", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } }, // mod+t terminal
+	{ MODKEY,                       XK_v,      spawn,          {.v = vimb } }, // mod+v vimb
+	{ MODKEY,                       XK_z,      spawn,          {.v = zathura } }, // mod+z zathura
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
